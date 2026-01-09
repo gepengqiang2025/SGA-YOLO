dcf211d30fe9dcdde5b2c170d6b698227822dc1c
Gepengqiang <pengqiang_ge@163.com>
2026-01-10 01:11:13 +0800

    Add files via upload

---
+++ SENetV2.py
@@ -0,0 +1,104 @@
+import torch
+import torch.nn as nn
+from .conv import Conv
+
+__all__ = ['C2f_SENetV2', 'SELayerV2']
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+class SELayerV2(nn.Module):
+    def __init__(self, in_channel, reduction=16):
+        super(SELayerV2, self).__init__()
+        assert in_channel >= reduction and in_channel % reduction == 0, 'invalid in_channel in SaElayer'
+        self.reduction = reduction
+        self.cardinality = 4
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.fc1 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fc3 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fc4 = nn.Sequential(
+            nn.Linear(in_channel, in_channel // self.reduction, bias=False),
+            nn.ReLU(inplace=True)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(in_channel // self.reduction * self.cardinality, in_channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y1 = self.fc1(y)
+        y2 = self.fc2(y)
+        y3 = self.fc3(y)
+        y4 = self.fc4(y)
+        y_concate = torch.cat([y1, y2, y3, y4], dim=1)
+        y_ex_dim = self.fc(y_concate).view(b, c, 1, 1)
+
+        return x * y_ex_dim.expand_as(x)
+
+
+class Bottleneck(nn.Module):
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+
+        super().__init__()
+        c_ = int(c2 * e)  
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.SEV2 = SELayerV2(c2)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        return x + self.SEV2(self.cv2(self.cv1(x))) if self.add else self.SEV2(self.cv2(self.cv1(x)))
+
+
+class C2f_SENetV2(nn.Module):
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+
+        super().__init__()
+        self.c = int(c2 * e)  
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
